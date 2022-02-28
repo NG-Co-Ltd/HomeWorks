e20215509a7dad043308888efd5507fc07c0bc5c
Nurbolot <ngulamidinov@gmail.com>
2022-03-01 01:08:00 +0600

    add Lesson2 Task001

--- Lesson002/Task001/Program.cs
+++ Lesson002/Task001/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿/* Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа. */
+
+System.Console.Write("Enter 3 digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = getTwoDigit(number);
+System.Console.WriteLine(number);
+int getTwoDigit(int number)
+{
+    number %= 100;
+    return number / 10;
+}
