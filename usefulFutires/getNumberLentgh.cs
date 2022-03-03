8497bc064075984405335955e976212d5c5bcbed
Nurbolot <ngulamidinov@gmail.com>
2022-03-03 20:40:14 +0600

    add usefulFutires - getNumberLength

---
+++ usefulFutires/getNumberLentgh.cs
@@ -0,0 +1,14 @@
+﻿int getNumberLength(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+    int numberLength = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        numberLength++;
+    }
+    return numberLength;
+}
