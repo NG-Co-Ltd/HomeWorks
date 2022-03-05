99b13fee8ab5a3e642ff3e1121ebdeedaaec8788
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 05:17:02 +0600

    add Lesson4 Task003 Calculating the product of numbers from 1 to n

---
+++ Lesson004/Task003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу вычисления произведения чисел от 1 до n
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine() ?? string.Empty);
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i} ");
+}
