ced323efa2514b91e1b57d1ca4197751addd0b28
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 03:40:23 +0600

    add Lesson4 Task001

--- Lesson004/Task001/Program.cs
+++ Lesson004/Task001/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Найти сумму чисел от 1 до а 
+
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine() ?? string.Empty);
+int result = findSumOf1ToN(number);
+Console.WriteLine($"Sum of 1 to {number} = {result}");
+
+int findSumOf1ToN(int number)
+{
+    return number * (number + 1) / 2;
+}
