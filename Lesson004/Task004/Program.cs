4b5bbc2ad96c62c367359c6fcce1cae6a72649b2
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 05:52:05 +0600

    add Lesson4 Task004 - set an array of 8 elements and display

---
+++ Lesson004/Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[] numbers = new int[8];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(100);
+}
+PrintArray(numbers);
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write(numbers[i] + " ");
+    }
+}
