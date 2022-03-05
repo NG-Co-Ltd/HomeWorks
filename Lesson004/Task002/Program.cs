998460b188f91f39cda87a13663cfbc2202fcdf8
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 04:39:09 +0600

    add Lesson4 Task002 - Determine the number of digit in a number

---
+++ Lesson004/Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Определить количество цифр в числе
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine() ?? string.Empty);
+
+int len = getNumberLength(number);
+Console.WriteLine($"The number of digits in a number: {len}");
+
+int getNumberLength(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+    int numberLength = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        numberLength++; 
+    }
+    return numberLength;
+}
