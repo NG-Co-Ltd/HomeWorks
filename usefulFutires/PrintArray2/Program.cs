840610030ef89c1d87bf6533ae945d3180f89a1b
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 01:43:31 +0600

    add PrintArray2 to UsefulFutires

---
+++ usefulFutires/PrintArray2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[,] array = new int[4, 10];
+PrintTwoDimensionalArray(array);
+
+void PrintTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
