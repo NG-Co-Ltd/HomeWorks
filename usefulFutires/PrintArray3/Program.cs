f694339333bed6375ecbf4a48876021e1d268f27
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 02:15:35 +0600

    add PrintArray3 to UsefulFutires

---
+++ usefulFutires/PrintArray3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,,] array = new int[3,5,4];
+printThreeDimensionArray(array);
+
+
+void printThreeDimensionArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+}
